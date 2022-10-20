314e402e180ae315cfd64dbaf0e4ba4e4b1ec78c
VanshikaNavya <66869020+VanshikaNavya@users.noreply.github.com>
2022-10-21 04:02:00 +0530

    Time: 35 ms (14.58%), Space: 9.3 MB (22.15%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<pair<string,int>> notations ={{"M",1000},{"CM",900},{"D",500},{"CD",400},{"C",100},{"XC",90},{"L",50},{"XL",40},{"X",10},{"IX",9},{"V",5},{"IV",4},{"I",1}};
+        string ans="";
+        
+        for(auto it: notations){
+            while(num>=it.second){
+                ans+=it.first;
+                num-=it.second;
+            }
+        }
+        return ans;
+    }
+};
