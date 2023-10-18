f332ea28d5cf77bbdf1f6f21614a2226f16ee236
VanshikaNavya <66869020+VanshikaNavya@users.noreply.github.com>
2023-10-18 14:32:49 +0530

    Time: 0 ms (100%), Space: 12.8 MB (39.96%) - LeetHub

--- 2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.cpp
+++ 2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int countKDifference(vector<int>& nums, int k) {
         unordered_map<int, int> mp;
@@ -9,4 +9,21 @@ public:
         }
         return count;
     }
+};*/
+
+//count sort
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) {
+        vector<int> v(101);
+        int count=0;
+        for(int i=0; i<nums.size(); i++){
+            v[nums[i]]++;    
+        }
+        for(int i=k; i<101; i++){
+            count+=v[i]*v[i-k];
+        }
+        return count;
+    }
 };
+
